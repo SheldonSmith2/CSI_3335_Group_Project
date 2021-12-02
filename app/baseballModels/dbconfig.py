d24da9fbaf04db6f2982aa07fdd61ba5ad9b564c
SheldonSmith2 <sheldon_smith2@baylor.edu>
2021-12-02 10:54:46 -0600

    Modifed ORM tables

---
+++ app/baseballModels/dbconfig.py
@@ -0,0 +1,12 @@
+mysql = {'host': 'localhost',
+         'user': 'web',
+         'password': 'dbrules',
+         'db': 'baseball'}
+root = {'host': 'localhost',
+        'user': 'root',
+        'password': 'csi3335rocks',
+        'db': 'baseball'}
+rootNew = {'host': 'localhost',
+        'user': 'root',
+        'password': 'csi3335rocks',
+        'db': 'Newbaseball'}
---
+++ finalCSVReaders/mariadbconfig.py
@@ -0,0 +1,8 @@
+mysql = {'host': 'localhost',
+         'user': 'web',
+         'password': 'dbrules',
+         'database': 'Newbaseall'}
+root =  {'host': 'localhost',
+         'user': 'root',
+         'password': 'csi3335rocks',
+         'database': 'Newbaseball'}
